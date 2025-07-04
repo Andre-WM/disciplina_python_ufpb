c7077ac714fc30bdf2f49d91116d671852b17120
Andre-WM <werlangandre30@gmail.com>
2025-07-04 10:58:04 -0300

    atualizacao do tipo de arquivo 1_Introducao

--- 1_Introducao.py
+++
@@ -1,7 +0,0 @@
-# criar um repositório no github para a disciplina
-
-# primeiro commit e add - > git add e git commit
-# git config --global user.email e user.name
-# consultar a branch -> git branch 
-# renomear a branch -> incompatibilidade entre master e main -> git branch -m main
-# conectar ao repositorio no github -> git remote add origin url
